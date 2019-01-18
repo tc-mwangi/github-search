947c4b184b9ea3b90b88f4d819ce52d6d40f85e9
Loise Mwangi <wanjirumwangi.learn@gmail.com>
2019-01-18 18:22:57 +0300

    add components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { SearchPageComponent } from './search-page/search-page.component';
+import { NavigationComponent } from './src/app/shared/navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    ProfilePageComponent,
+    SearchPageComponent,
+    NavigationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
